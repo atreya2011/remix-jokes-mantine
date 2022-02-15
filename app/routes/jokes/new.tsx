452def95884fd2dbd8cae63b0681c33ae7320c2f
Atreya <44151328+atreya2011@users.noreply.github.com>
2022-02-15 15:59:22 +0900

    fix: add basic form validation

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,21 +1,55 @@
 import { Button, Container, Paper, Text, Textarea, TextInput } from "@mantine/core";
-import { ActionFunction, redirect } from "remix";
+import { ActionFunction, json, redirect, useActionData } from "remix";
 import { db } from "~/utils/db.server";
+
+function validateJokeContent(content: string) {
+  if (content.length < 3) {
+    return "Joke content must be at least 3 characters long";
+  }
+}
+
+function validateJokeName(name: string) {
+  if (name.length < 3) {
+    return "Joke's name must be at least 3 characters long";
+  }
+}
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name?: string;
+    content?: string;
+  };
+  fields?: {
+    name: string;
+    content: string;
+  };
+};
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
-  console.log(name, content);
   if (typeof name !== "string" || typeof content !== "string") {
-    throw new Error("Invalid form data");
+    return badRequest({ formError: "Missing name or content" });
   }
+  const fieldErrors = {
+    name: validateJokeName(name),
+    content: validateJokeContent(content),
+  };
   const fields = { name, content };
+  if (Object.values(fieldErrors).some((error) => error !== undefined)) {
+    return badRequest({ fieldErrors, fields });
+  }
   const joke = await db.joke.create({ data: fields });
   return redirect(`${joke.id}`);
 };
 
 export default function NewJokeRoute() {
+  const actionData = useActionData<ActionData>();
+
   return (
     <Container>
       <Paper padding={"xs"}>
@@ -23,10 +57,22 @@ export default function NewJokeRoute() {
       </Paper>
       <form method="post">
         <Paper padding={"xs"}>
-          <TextInput name="name" placeholder="Name" label="Name" required />
+          <TextInput
+            name="name"
+            placeholder="Name"
+            label="Name"
+            required
+            error={actionData?.fieldErrors?.name ? actionData.fieldErrors.name : null}
+          />
         </Paper>
         <Paper padding={"xs"}>
-          <Textarea name="content" placeholder="Your comment" label="Your comment" required />
+          <Textarea
+            name="content"
+            placeholder="Your content"
+            label="Your content"
+            required
+            error={actionData?.fieldErrors?.content ? actionData.fieldErrors.content : null}
+          />
         </Paper>
         <Paper padding={"xs"}>
           <Button type="submit">Submit</Button>
